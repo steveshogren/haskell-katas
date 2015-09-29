6ae620262426496155a051e1d32701f2bd1cf417
Jack <steve.a.shogren@gmail.com>
2015-09-29 17:14:56 -0400

    Adding the ability to parse multi-line messages

--- src/BankOCR.hs
+++ src/BankOCR.hs
@@ -5,15 +5,17 @@ import Data.List.Split(chunksOf)
 import System.Random
 import Control.Applicative
 
-getFile :: FilePath -> IO [String]
-getFile name = liftM (take 3 . lines) $ readFile name
+-- data Number = Number String String String
+
+getFile :: FilePath -> IO [[String]]
+getFile name = liftM (chunksOf 4 . lines) $ readFile name
 
 breakIntoThrees :: [String] -> [[String]]
 breakIntoThrees = map (chunksOf 3)
 
-makeDigitTable [[], [], []] = []
-makeDigitTable [a, b, c] =
-  [[head a, head b, head c]] ++ makeDigitTable [tail a, tail b, tail c]
+makeDigitTable [[], [], [], []] = []
+makeDigitTable [a, b, c, _] =
+  [[head a, head b, head c]] ++ makeDigitTable [tail a, tail b, tail c, []]
 
 matchWith :: Num a => [[Char]] -> a
 matchWith [" _ ",
@@ -46,6 +48,30 @@ matchWith [" _ ",
 matchWith [" _ ",
            "|_|",
            "  |"] = 9
+matchWith x = error $ show x
+
+checkSum :: Integral a => [a] -> Bool
+checkSum [d9,d8,d7,d6,d5,d4,d3,d2,d1] =
+   let x = d1 + (2 * d2) + (3 * d3) + (4 * d4) + (5 * d5) + (6 * d6) + (7 * d7) + (8* d8) + (9 * d9)
+   in mod x 11 == 0
+checkSum _ = False
+
+makeOutput x =
+    let msg = if checkSum x then "    " else " ERR"
+    in (foldr (++) " " $ map show x) ++ msg
+
+parse = makeOutput . map matchWith . makeDigitTable . breakIntoThrees
+
+doer = do
+  x <- getFile "input.dt"
+  return $ map parse x
+
+
+
+
+
+
+-- Random kata stuff
 
 fib 0 = 0
 fib 1 = 1
@@ -55,19 +81,6 @@ fib2 =
   let fibi a b n = if (n > 0) then fibi b (a+b) (n-1) else a
   in fibi 0 1
 
-checkSum :: Integral a => [a] -> Bool
-checkSum [d9,d8,d7,d6,d5,d4,d3,d2,d1] = 
-   let x = d1 + (2 * d2) + (3 * d3) + (4 * d4) + (5 * d5) + (6 * d6) + (7 * d7) + (8* d8) + (9 * d9)
-   in mod x 11 == 0
-checkSum _ = False
-
-makeOutput x =
-    let msg = if checkSum x then "    " else " ERR"
-    in (foldr (++) " " $ map show x) ++ msg
-
-doer = do
-  x <- getFile "input.dt"
-  return . makeOutput . map matchWith . makeDigitTable . breakIntoThrees $ x
 
 shuffle :: [a] -> IO [a]
 shuffle [] = return []
