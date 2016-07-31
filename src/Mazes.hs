1f67be5e23f5f47882effb3d8f86af53b5829cc3
jack <steve.a.shogren@gmail.com>
2016-07-31 13:19:42 -0400

    Printing the map

--- src/Mazes.hs
+++ src/Mazes.hs
@@ -4,18 +4,36 @@ module Mazes where
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 
-type Cell = (Int,Int)
+type Cell = (Integer,Integer)
 type Board = Map.Map Cell Bool
 
+emptyBoard =
+  let m = Map.empty
+      coords = [(x,y) | x <- [0..20], y <- [0..7]]
+  in foldl (\m k -> Map.insert k False m) m coords
+
+sampleBoard =
+  let m = emptyBoard
+  in foldl (\m k -> Map.insert k True m) m [(0,0), (1,0), (2,0), (4,0), (1,2) ]
+
+toChar :: Bool -> String
 toChar x = if x then "a" else " "
+
 toBool = Maybe.fromMaybe False
 
-toStringMap :: Board -> String
-toStringMap m l w =
+toStringRow :: Board -> Integer -> String
+toStringRow m y =
   let ks = Map.keys m
-      topRow = filter ((==0).snd) ks
-      topString = foldl (\ret k -> ret++((toChar . toBool . Map.lookup m) k)) "" topRow
+      topRow = filter ((==y).snd) ks
+      topString = foldl (\ret k -> ret++((toChar . toBool . Map.lookup k) m)) "" topRow
   in topString
+
+toStringMap :: Board -> [String]
+toStringMap m =
+  map (\y -> toStringRow m y) [0..7]
+
+printMap =
+  sequence . (map putStrLn) $ toStringMap sampleBoard
 
 generate = do
   putStrLn "abcdef                           "
