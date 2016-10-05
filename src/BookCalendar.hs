76f70266b0314eb13747788a53463a23375dc5bf
jack <steve.a.shogren@gmail.com>
2016-10-05 06:58:49 -0400

    Clean the calendar up for the book

--- src/BookCalendar.hs
+++ src/BookCalendar.hs
@@ -1,17 +1,18 @@
 module BookCalendar where
 
 import Data.Time
+import Data.Time.Calendar.WeekDate
 
 currentWordCount :: Integer
-currentWordCount = 9048
+currentWordCount = 11326
 
 wordsPerDay :: Integer
 wordsPerDay = 333
 
-isWeekend :: FormatTime t => t -> Bool
+isWeekend :: Day -> Bool
 isWeekend d =
-  let da = formatTime defaultTimeLocale "%a" d
-  in da == "Sat" || da == "Sun"
+  let (_,_,dow) = toWeekDate d
+  in dow > 5
 
 addToDay :: UTCTime -> Integer -> Day
 addToDay today days = addDays days . utctDay $ today
@@ -19,14 +20,19 @@ addToDay today days = addDays days . utctDay $ today
 printDay :: FormatTime t => t -> String
 printDay d = formatTime defaultTimeLocale "   %a - %b %e %Y" d
 
-buildDate :: Integer -> UTCTime -> Integer -> [Char]
-buildDate goal today daysFuture =
-  let dayNumber = addToDay today daysFuture
-  in (show ((goal * daysFuture) + currentWordCount)) ++ printDay dayNumber
+futureCounts :: [Integer]
+futureCounts = map (\n -> currentWordCount + (n * wordsPerDay)) [1..]
 
-dailyCounts :: Integer -> UTCTime -> [[Char]]
+buildDate :: UTCTime -> Integer -> String
+buildDate today daysFuture =
+  let dayNumber = addToDay today daysFuture
+  in printDay dayNumber
+
+dailyCounts :: t -> UTCTime -> [String]
 dailyCounts goal today =
- fmap (buildDate goal today) $ filter (\n -> not $ isWeekend $ addToDay today n ) [1..35]
+ let days = filter (\n -> not $ isWeekend $ addToDay today n) [1..35]
+     dayStrings = map (buildDate today) days
+ in map (\(n, d) -> (show n) ++ d) $ zip futureCounts dayStrings
 
 main :: IO [()]
 main = do
