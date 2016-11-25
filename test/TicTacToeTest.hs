65f05abf25a4abd12678508d4b243aa025537cac
jack <steve.a.shogren@gmail.com>
2016-11-25 06:55:38 -0500

    Testing the win conditions conclusively

--- test/TicTacToeTest.hs
+++ test/TicTacToeTest.hs
@@ -40,6 +40,32 @@ testWinning =
                         " XX"]
   in (assertEqual "Player 0 Winning" expectedString game)
 
+testDidWin :: Game -> Bool
+testDidWin (Unfinished a) = didWin a
+testDidWin _ = False
+
+testWinConditions :: Assertion
+testWinConditions =
+  let f = NoMoves
+      topRowWin = testDidWin $ f >>== topLeft >>== bottomCen >>== topCen >>== bottomRight >>== topRight
+      midRowWin = testDidWin $ f >>== midLeft >>== bottomCen >>== midCen >>== bottomRight >>== midRight
+      bottomRowWin = testDidWin $ f >>== bottomLeft >>== topCen >>== bottomCen >>== midRight >>== bottomRight
+      rightWin = testDidWin $ f >>== bottomRight >>== topCen >>== midRight >>== bottomLeft >>== topRight
+      leftWin = testDidWin $ f >>== bottomLeft >>== topCen >>== midLeft >>== bottomRight >>== topLeft
+      cenWin = testDidWin $ f >>== bottomCen >>== topRight >>== midCen >>== bottomRight >>== topCen
+      noWin = testDidWin $ f >>== bottomCen >>== topRight >>== midLeft >>== bottomRight >>== topCen
+      diagLeft = testDidWin $ f >>== bottomLeft >>== topCen >>== midCen >>== bottomRight >>== topRight
+      diagRight = testDidWin $ f >>== bottomRight >>== topRight >>== midCen >>== bottomRight >>== topLeft
+  in (assertEqual "Top row win" True topRowWin)
+     >> (assertEqual "Mid row win" True midRowWin)
+     >> (assertEqual "Bottom row win" True bottomRowWin)
+     >> (assertEqual "Right win" True rightWin)
+     >> (assertEqual "left win" True leftWin)
+     >> (assertEqual "cen win" True cenWin)
+     >> (assertEqual "no win" False noWin)
+     >> (assertEqual "diag right" False diagRight)
+     >> (assertEqual "diag left" False diagLeft)
+
 testUnfinished :: Assertion
 testUnfinished =
   let f = NoMoves
@@ -55,7 +81,8 @@ tests = testGroup "TicTacToeTests"
     testCase "undo game one move" testUndoOnlyOneMove,
     testCase "undo game" testUndo,
     testCase "game unfinished" testUnfinished,
-    testCase "winning game" testWinning
+    testCase "winning game" testWinning,
+    testCase "Win conditions" testWinConditions
   ]
 
 main :: IO ()
