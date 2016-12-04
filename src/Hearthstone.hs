ab845c34670bfc01c0397c9574297e3ca0a7cf9c
jack <steve.a.shogren@gmail.com>
2016-12-04 07:23:17 -0500

    Making the deck shuffling

--- src/Hearthstone.hs
+++ src/Hearthstone.hs
@@ -1,5 +1,24 @@
 module Hearthstone where
 
 import qualified Data.Map as M
+import qualified System.Random.Shuffle as Rand
+import Control.Monad.Random
+import Control.Monad
 
-x = 1
+type Health = Int
+type Mana = Int
+data Player = Player Health Mana
+
+type Card = Int
+type Deck = [Card]
+
+fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
+
+makeDeck :: (MonadRandom m) => m Deck
+makeDeck = Rand.shuffleM fullDeck
+
+main = do
+  a <- makeDeck
+  b <- makeDeck
+  putStrLn $ show a
+  putStrLn $ show b
