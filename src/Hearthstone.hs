b028a1159ec68a6b9503f12b63696eae8ca293c9
jack <steve.a.shogren@gmail.com>
2016-12-05 07:23:17 -0500

    Attempting haskell tdd!

--- src/Hearthstone.hs
+++ src/Hearthstone.hs
@@ -5,18 +5,23 @@ import qualified System.Random.Shuffle as Rand
 import Control.Monad.Random
 import Control.Monad
 
+type Card = Int
+type Deck = [Card]
+type Hand = [Card]
+
 type Health = Int
 type Mana = Int
 data Player = Player Health Mana
 
-type Card = Int
-type Deck = [Card]
-
+fullDeck :: [Card]
 fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
 
 makeDeck :: (MonadRandom m) => m Deck
 makeDeck = Rand.shuffleM fullDeck
 
+playHand p1 p2 h1 = p1 p2
+
+main :: IO ()
 main = do
   a <- makeDeck
   b <- makeDeck
--- test/Hearthstone.hs
+++ test/Hearthstone.hs
@@ -4,15 +4,19 @@ import Hearthstone
 import Test.Tasty
 import Test.Tasty.HUnit
 
-testUndoOnlyOneMove :: IO ()
-testUndoOnlyOneMove =
-  assertEqual "Undoing the move" 1 1
+testPlayingAHand :: IO ()
+testPlayingAHand =
+  let p1 = Player 30 1 []
+      p2 = Player 30 1 []
+      h1 = [0,0,1,1]
+      (p1a, p2a) = playHand p1 p2 h1
+  in assertEqual "Undoing the move" 1 1
 
 
 tests :: TestTree
 tests = testGroup "HeartstoneTests"
   [
-    testCase "undo game one move" testUndoOnlyOneMove
+    testCase "undo game one move" testPlayingAHand
   ]
 
 main :: IO ()
