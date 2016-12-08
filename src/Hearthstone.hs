618832eefe6ba5b279d1c21306855c2a110262b3
jack <steve.a.shogren@gmail.com>
2016-12-08 07:03:09 -0500

    Make the game play any cards it can

--- src/Hearthstone.hs
+++ src/Hearthstone.hs
@@ -11,7 +11,7 @@ type Hand = [Card]
 
 type Health = Int
 type Mana = Int
-data Player = Player Health Mana
+data Player = Player Health Mana Hand
 
 fullDeck :: [Card]
 fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
@@ -19,8 +19,17 @@ fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
 makeDeck :: (MonadRandom m) => m Deck
 makeDeck = Rand.shuffleM fullDeck
 
-playHand :: t -> t1 -> t2 -> (t, t1, t2)
-playHand p1 p2 h1 = (p1, p2, h1)
+playHand :: Player -> Player -> (Player, Player)
+playHand (Player health1 mana1 cards1) (Player health2 p2m p2c) =
+  let (totalDamage, handLeft, manaLeft) =
+        foldr
+        (\card (dam, hand, mana) ->
+           if (card >= manaLeft) then
+             (dam+card, hand,mana-card)
+           else
+             (dam, [card]++hand,mana))
+        (0, [], mana1) cards1
+  in (Player health1 manaLeft handLeft, Player (health2 - totalDamage) p2m p2c)
 
 main :: IO ()
 main = do
--- test/HearthstoneTest.hs
+++ test/HearthstoneTest.hs
@@ -4,13 +4,13 @@ import Hearthstone
 import Test.Tasty
 import Test.Tasty.HUnit
 
-testPlayingAHand :: IO ()
 testPlayingAHand =
-  let p1 = Player 30 1
-      p2 = Player 30 1
-      h1 = [0,0,1,1]
-      (p1a, p2a, p1h) = playHand p1 p2 h1
-  in assertEqual "Undoing the move" 1 1
+  let p1 = Player 30 1 [0,0,1,1]
+      p2 = Player 30 1 []
+      (Player p1h _ p1hand, Player p2h _ _) = playHand p1 p2
+  in (assertEqual "health" 30 p1h)
+     >> (assertEqual "hand" [0,0,1] p1hand )
+     >> (assertEqual "p2 health" 29 p2h)
 
 
 tests :: TestTree
