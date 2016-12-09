37e8d05e08737c7657769f343c56e62e26e62abf
jack <steve.a.shogren@gmail.com>
2016-12-09 14:24:05 -0500

    Playing a whole round

--- src/Hearthstone.hs
+++ src/Hearthstone.hs
@@ -10,7 +10,7 @@ type Hand = [Card]
 
 type Health = Int
 type Mana = Int
-data Player = Player Health Mana Hand
+data Player = Player Health Mana Hand Deck
 
 fullDeck :: [Card]
 fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
@@ -18,18 +18,28 @@ fullDeck = [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8]
 makeDeck :: (MonadRandom m) => m Deck
 makeDeck = Rand.shuffleM fullDeck
 
-playHand :: Player -> Player -> (Player, Player)
-playHand (Player health1 mana1 cards1) (Player health2 p2m p2c) =
-  let (totalDamage, handLeft, manaLeft) =
+playHand :: (Player, Player) -> (Player, Player)
+playHand ((Player health1 mana1 cards1 d1), (Player health2 p2m p2c d2)) =
+  let newCard = head d1
+      newDeck = drop 1 d1
+      (totalDamage, handLeft, manaLeft) =
         foldr
         (\card (dam, hand, mana) ->
            if (card <= mana) then
              (dam+card, hand, mana-card)
            else
-             (dam, [card]++hand,mana))
+             (dam, card:hand,mana))
         (0, [], mana1)
-        (sort cards1)
-  in (Player health1 manaLeft handLeft, Player (health2 - totalDamage) p2m p2c)
+        (sort (newCard:cards1))
+      p2 = Player (health2 - totalDamage) p2m p2c d2
+      p1 = Player health1 manaLeft handLeft newDeck
+  in (p2, p1)
+
+playRound :: (Player, Player) -> (Player, Player)
+playRound a =
+  let (p2, p1) = playHand a
+      (p1a, p2a) = playHand (p2,p1)
+  in (p1a, p2a)
 
 main :: IO ()
 main = do
--- test/HearthstoneTest.hs
+++ test/HearthstoneTest.hs
@@ -6,16 +6,27 @@ import Test.Tasty.HUnit
 
 testPlayingAHand :: IO ()
 testPlayingAHand =
-  let p1 = Player 30 6 [5,0,0,1,1]
-      p2 = Player 30 1 []
-      (Player p1h _ p1hand, Player p2h _ p2hand) = playHand p1 p2
+  let p1 = Player 30 6 [5,0,1,1] [0]
+      p2 = Player 30 1 [] []
+      ( Player p2h _ p2hand _,Player p1h _ p1hand _) = playHand(p1, p2)
   in (assertEqual "health" 30 p1h)
      >> (assertEqual "p2 health" 24 p2h)
      >> (assertEqual "hand" [1] p1hand )
      >> (assertEqual "p2 hand" [] p2hand)
 
+testPlayingARound :: IO ()
+testPlayingARound =
+  let p1 = Player 30 6 [5,0,0,1,1] [0]
+      p2 = Player 30 6 [5,1,1,1,2] [0]
+      (Player p1h _ p1hand _, Player p2h _ p2hand _) = playRound(p1, p2)
+  in (assertEqual "health" 24 p1h)
+     >> (assertEqual "p2 health" 24 p2h)
+     >> (assertEqual "hand" [1] p1hand)
+     >> (assertEqual "p2 hand" [1,1,2] p2hand)
+
 tests :: TestTree
 tests = testGroup "HeartstoneTests"
   [
-    testCase "playing just one card" testPlayingAHand
+    testCase "playing just one card" testPlayingAHand,
+    testCase "playing just one round" testPlayingARound
   ]
