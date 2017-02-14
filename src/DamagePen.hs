248616c09542b40e2ea383394f8bd2e4a846b327
jack <steve.a.shogren@gmail.com>
2017-02-14 06:50:48 -0500

    Switching out the lib for Data.LinearProgram.GLPK

--- src/DamagePen.hs
+++ src/DamagePen.hs
@@ -1,46 +1,34 @@
 module DamagePen where
 
-import Math.LinearEquationSolver
-
--- 2x + 3y + 4z = 20
--- 6x - 3y + 9z = -6
--- 2x      +  z = 8
--- x = solveIntegerLinearEqs Z3 [[2, 3, 4],[6, -3, 9],[2, 0, 1]] [20, -6, 8] 
-
 attackSpeed :: Fractional a => a -> a -> a
 attackSpeed bat asm =(1/bat)*asm
 
-yesDoubleCrit :: Double
-yesDoubleCrit = 2.5
+yesBonusCrit :: Double
+yesBonusCrit = 2.5
 
-noDoubleCrit :: Double
-noDoubleCrit = 1.5
+noBonusCrit :: Double
+noBonusCrit = 1.5
 
+armorPointsToNum :: Num a => a -> a
 armorPointsToNum ar = ar * 7
 
 dmgReduction :: Fractional a => a -> a -> a -> a
 dmgReduction ar pn lvl =
-  let armor = (armorPointsToNum ar) + 31.5
+  let armor = ar + 31.5
       pen = pn * 4.0
   in (armor-pen) / (100+(armor-pen)+(10*(lvl-1)))
 
 murdockDps :: Double
-murdockDps = dps 20 14 18 8 yesDoubleCrit 86 1.35 1 15
+murdockDps = dps 20 14 18 8 yesBonusCrit 86 1.35 1 15
+
+-- pp + att_spd + crit_points = 60
 
 dps :: (Fractional a) => a -> a -> a -> a -> a -> a -> a -> a -> a -> a
 dps power_points attack_speed_points crit_points pen crit_damage base_damage base_attack_speed scaling lvl =
   let reduction = dmgReduction 0 pen lvl
-      dps = (base_damage+6*power_points*scaling)
+      raw_dps = (base_damage+6*power_points*scaling)
               * ((1+(0.055*attack_speed_points))/base_attack_speed)
               * (1+(0.04*crit_points)*(crit_damage-1))
-  in dps * (1 - reduction)
+  in raw_dps * (1 - reduction)
 
-calcDamagef :: Num a => a -> a -> a -> a
-calcDamagef bp arc ar =
-  calcDamage bp arc ar 0 0 0 0 0 0
-
-calcDamage
-  :: Num a => a -> a -> a -> a -> a -> a -> a -> a -> a -> a
-calcDamage bp arc ar aoerating dbs dbt dr pdr edr =
-  ( bp  + (arc * ar ) ) * aoerating * ( dbs ) * ( dbt ) * ( 1 - dr ) * ( 1 - pdr ) * ( 1 - edr )
 
