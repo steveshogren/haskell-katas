4f9d2b1003dd6bdeba9954ceb113fa31edac6bfd
jack <steve.a.shogren@gmail.com>
2017-04-28 07:05:27 -0400

    Switch to using Data.Map

--- src/DiceRoller.hs
+++ src/DiceRoller.hs
@@ -1,43 +1,37 @@
 module DiceRoller where
 
 import Control.Monad.Random
-import Control.Monad
+import qualified Data.Map as M
 
 die :: RandomGen g => Rand g Int
 die = getRandomR (1,6)
 
+roll :: IO Int
 roll = do
   roll1 <- evalRandIO $ die
   roll2 <- evalRandIO $ die
-  if (roll2 > 4) then
+  if (roll2 == 6) then
     return 6
   else return roll1
 
 roller :: Int -> IO [Int]
 roller n = sequence (replicate n roll)
 
-data RollCounts = RollCounts { one :: Int, two :: Int, three::Int, four::Int,five::Int,six::Int}
-  deriving Show
-newHash = RollCounts { one = 0, two = 0, three = 0, four = 0, five = 0, six = 0}
+emptyMap :: M.Map Int Int
+emptyMap = M.empty
 
+main :: IO ()
 main = do
-  rolls <- roller 100
+  let rollCount = 10000
+  rolls <- roller rollCount
   let counts = foldr (\next result ->
-                        if (next == 1) then
-                          result { one = (one result) + 1 }
-                        else if (next == 2) then
-                          result { two = (two result) + 1 }
-                        else if (next == 3) then
-                          result { three = (three result) + 1 }
-                        else if (next == 4) then
-                          result { four = (four result) + 1 }
-                        else if (next == 5) then
-                          result { five = (five result) + 1 }
-                        else
-                          result { six = (six result) + 1 }
-                        ) newHash rolls
-  let total = (one counts) + (two counts) + (three counts) + (four counts) + (five counts) + (six counts) 
-  let expectedAverage = (fromIntegral total) / (fromIntegral 6)
-  let sixWeight = ((fromIntegral (six counts)) / expectedAverage) * 100
+                          let current = M.findWithDefault 0 next result
+                          in M.insert next (current+1) result
+                        ) emptyMap rolls
+  let expectedAverage = (fromIntegral rollCount) / 6
+  let sixWeight = ((fromIntegral (M.findWithDefault 0 6 counts)) / expectedAverage) * 100
+  if sixWeight > 105 || sixWeight < 98 then
+    print "Its UNFAIR"
+  else print "Seems legit"
   print counts
   print sixWeight
