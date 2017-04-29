219a8796006540081aff95ea4946a59f62f6ba7c
jack <steve.a.shogren@gmail.com>
2017-04-29 08:33:16 -0400

    Using lens to change the fields

--- src/DiceRoller.hs
+++ src/DiceRoller.hs
@@ -2,6 +2,9 @@ module DiceRoller where
 
 import Control.Monad.Random
 import qualified Data.Map as M
+import Control.Lens
+import Data.Map.Lens
+import Control.Lens.Iso
 
 die :: RandomGen g => Rand g Int
 die = getRandomR (1,6)
@@ -20,18 +23,19 @@ roller n = sequence (replicate n roll)
 emptyMap :: M.Map Int Int
 emptyMap = M.empty
 
+
+doer :: M.Map Int Int -> M.Map Int Int
+doer = at 0 . non 0 +~ 1
+
 main :: IO ()
 main = do
   let rollCount = 10000
   rolls <- roller rollCount
-  let counts = foldr (\next result ->
-                          let current = M.findWithDefault 0 next result
-                          in M.insert next (current+1) result
-                        ) emptyMap rolls
+  let counts = foldr (\next result -> at next . non 0 +~ 1 $ result) emptyMap rolls
   let expectedAverage = (fromIntegral rollCount) / 6
-  let sixWeight = ((fromIntegral (M.findWithDefault 0 6 counts)) / expectedAverage) * 100
+  let sixWeight = ((fromIntegral (counts ^.at 6 . non 0)) / expectedAverage) * 100
   if sixWeight > 105 || sixWeight < 98 then
-    print "Its UNFAIR"
+    print $ "Its UNFAIR" ++ " six showed up " ++ show sixWeight ++ "% of the time!"
   else print "Seems legit"
   print counts
   print sixWeight
