761f69e1f7ba5d1a914243e37244075d1b8c779c
jack <steve.a.shogren@gmail.com>
2017-07-02 08:28:47 -0400

    Simple cases

--- src/RomanNumeral.hs
+++ src/RomanNumeral.hs
@@ -3,3 +3,6 @@ module RomanNumeral where
 add x y = x + y
 
 
+toRoman 1 = "I"
+toRoman 5 = "V"
+toRoman 10 = "X"
--- test/RomanNumeralTest.hs
+++ test/RomanNumeralTest.hs
@@ -9,10 +9,17 @@ testUnfinished =
   let f = add 1 2
   in (assertEqual "Add 1 2" f 3)
 
+testToRoman :: Assertion
+testToRoman =
+  (assertEqual "" (toRoman 1) "I")
+  >> (assertEqual "" (toRoman 5) "V")
+  >> (assertEqual "" (toRoman 10) "X")
+
 tests2 :: TestTree
 tests2 = testGroup "RomanNumeralTests"
   [
-    testCase "simple addition" testUnfinished
+    testCase "simple addition" testUnfinished,
+    testCase "toRoman" testToRoman
   ]
 
 runner = defaultMain tests2
