e6d4a67971b9279c925f953f115b17086e78d639
jack <steve.a.shogren@gmail.com>
2017-07-04 06:41:23 -0400

    Solving up to 39

--- src/RomanNumeral.hs
+++ src/RomanNumeral.hs
@@ -1,13 +1,19 @@
 module RomanNumeral where
 
+add :: Num a => a -> a -> a
 add x y = x + y
 
 
+toRoman :: Int -> String
 toRoman 1 = "I"
 toRoman 5 = "V"
 toRoman 10 = "X"
 toRoman arabic =
-  if (arabic > 5) then
+  if (arabic > 10) then
+    (toRoman 10) ++ (toRoman (arabic - 10))
+  else if (arabic > 8) then
+    (toRoman 1) ++ (toRoman 10)
+  else if (arabic > 5) then
     (toRoman 5) ++ (toRoman (arabic - 5))
   else if (arabic > 3) then
     (toRoman 1) ++ (toRoman 5)
--- test/RomanNumeralTest.hs
+++ test/RomanNumeralTest.hs
@@ -11,13 +11,18 @@ testUnfinished =
 
 testToRoman :: Assertion
 testToRoman =
-  (assertEqual ""    "I" (toRoman 1)  )
-  >> (assertEqual "" "V" (toRoman 5)  )
-  >> (assertEqual "" "X" (toRoman 10) )
-  >> (assertEqual "" "II"(toRoman 2)  )
-  >> (assertEqual "" "IV"(toRoman 4)  )
-  >> (assertEqual "" "VI"(toRoman 6)  )
-  >> (assertEqual "" "VIII"(toRoman 8)  )
+  (assertEqual "" "I" (toRoman 1))
+  >> (assertEqual "" "V" (toRoman 5))
+  >> (assertEqual "" "X" (toRoman 10))
+  >> (assertEqual "" "II" (toRoman 2))
+  >> (assertEqual "" "IV" (toRoman 4))
+  >> (assertEqual "" "VI" (toRoman 6))
+  >> (assertEqual "" "VIII" (toRoman 8))
+  >> (assertEqual "" "IX" (toRoman 9))
+  >> (assertEqual "" "XI" (toRoman 11))
+  >> (assertEqual "" "XXXVIII" (toRoman 38))
+  >> (assertEqual "" "XXXVIII" (toRoman 38))
+  >> (assertEqual "" "XXXIX" (toRoman 39))
 
 tests2 :: TestTree
 tests2 = testGroup "RomanNumeralTests"
