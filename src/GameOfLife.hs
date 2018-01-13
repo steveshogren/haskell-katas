e9eb591a2991a0e103f0a5c7a13a0f9994b3d789
jack <steve.a.shogren@gmail.com>
2018-01-13 06:16:16 -0500

    Pass 2/3 cases

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -2,10 +2,16 @@ module GameOfLife where
 
 import qualified Data.Map as M
 
-data Cell = Cell State Int Int
+data Cell = Cell Bool Int Int
   deriving(Show,Eq,Ord)
 
-data State = Alive | Dead
-  deriving(Show,Eq,Ord)
+xor True False = True
+xor False True = True
+xor _ _ = False
 
-liveNeighborCount board (Cell _ x y) = 1
+liveNeighborCount board (Cell _ x y) =
+  length $ filter (\(Cell isLive bx by) ->
+                     let xClose = (abs (bx-x)) < 2
+                         yClose = (abs (by-y)) < 2
+                         isntSelf = not ((bx/= x) && (by/= y))
+                      in xClose && yClose && isntSelf && isLive) board
--- test/GameOfLifeTest.hs
+++ test/GameOfLifeTest.hs
@@ -4,13 +4,13 @@ import GameOfLife
 import Test.Tasty
 import Test.Tasty.HUnit
 
-aBoard = [Cell Alive 0 0, Cell Alive 1 1]
+aBoard = [Cell True 0 0, Cell True 1 1, Cell False 2 1, Cell False 1 2, Cell False 2 3, Cell False 3 2]
 
 testNeighbors :: Assertion
 testNeighbors =
-   (assertEqual "T1" 2 (liveNeighborCount aBoard $ Cell Alive 0 1))
-   >> (assertEqual "T2" 2 (liveNeighborCount aBoard $ Cell Alive 1 0))
-   >> (assertEqual "T3" 1 (liveNeighborCount aBoard $ Cell Alive 2 2))
+   (assertEqual "T1" 2 (liveNeighborCount aBoard $ Cell True 0 1))
+   >> (assertEqual "T2" 2 (liveNeighborCount aBoard $ Cell True 1 0))
+   >> (assertEqual "T3" 1 (liveNeighborCount aBoard $ Cell True 2 2))
 
 tests2 :: TestTree
 tests2 = testGroup "GameOfLifeTests"
