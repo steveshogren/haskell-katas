b242206ae50d9bc413bb63f453361361936b8a21
jack <steve.a.shogren@gmail.com>
2018-01-15 07:12:16 -0500

    Playing game, but with a bug

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -1,14 +1,18 @@
 module GameOfLife where
 
-import qualified Data.Map as M
+import Control.Concurrent
+
+-- import qualified Data.Map as M
 
 data Cell = Cell Bool Int Int
   deriving(Show,Eq,Ord)
 
+xor :: Bool -> Bool -> Bool
 xor True False = True
 xor False True = True
 xor _ _ = False
 
+liveNeighborCount :: [Cell] -> Cell -> Int
 liveNeighborCount board (Cell _ x y) =
   length $ filter (\(Cell isLive bx by) ->
                      let xClose = (abs (bx-x)) < 2
@@ -16,13 +20,36 @@ liveNeighborCount board (Cell _ x y) =
                          self = (bx== x) && (by== y)
                       in xClose && yClose && (not self) && isLive) board
 
-stepCell board (Cell s x y) =
-   let liveCount = liveNeighborCount board (Cell s x y)
+stepCell :: [Cell] -> Cell -> Cell
+stepCell board cell@(Cell s x y) =
+   let liveCount = liveNeighborCount board cell
    in if liveCount < 2 || (liveCount > 3 && s)
    then Cell False x y
    else if liveCount == 3 && (not s)
         then Cell True x y
-        else Cell s x y
+        else cell
 
+stepBoard :: [Cell] -> [Cell]
 stepBoard board =
-  map stepCell board
+  map (stepCell board) board
+
+status :: Foldable t => t Cell -> [Int] -> Bool
+status board [x,y] =
+  foldl (\ret (Cell s cx cy) -> if (cx == x) && (cy==y) then s else ret) False board
+
+printBoard :: Foldable t => t Cell -> Int -> IO ()
+printBoard board size =
+  let empty = [[x,y] | x <- [0..size], y <- [0..size]]
+  in putStr $ "----\n" ++ concatMap (\cell@[x,y] ->
+                  let live = status board cell
+                      cellString = if live then "|X" else"| "
+                  in if y == size then cellString ++ "\n" else cellString) empty
+
+playGame board size = do
+  printBoard board size >> threadDelay 1000000
+  let newBoard = stepBoard board
+  playGame newBoard size
+
+
+bBoard :: [Cell]
+bBoard = [Cell True 2 1, Cell True 1 1, Cell True 0 1]
--- test/GameOfLifeTest.hs
+++ test/GameOfLifeTest.hs
@@ -19,15 +19,20 @@ testStepBoard =
    let dieUnderpop = [Cell False 0 0, Cell True 0 1]
        dieOverpop = [Cell True 0 0, Cell True 0 1, Cell True 1 1, Cell True 2 0, Cell True 1 0]
        growExactPop = [Cell True 0 0, Cell True 0 1, Cell True 1 1, Cell False 1 0]
+       twoLives = [Cell True 0 0, Cell True 0 1, Cell False 1 1, Cell True 1 0]
+       threeLives = [Cell True 0 0, Cell True 0 1, Cell True 1 1, Cell True 1 0]
    in (assertEqual "S1" (Cell False 0 1) (stepCell dieUnderpop (Cell True 0 1)))
       >> (assertEqual "S2" (Cell False 1 0) (stepCell dieOverpop (Cell True 1 0)))
       >> (assertEqual "S3" (Cell True 1 0) (stepCell growExactPop (Cell False 1 0)))
+      >> (assertEqual "S4" (Cell True 1 0) (stepCell twoLives (Cell True 1 0)))
+      >> (assertEqual "S5" (Cell True 1 0) (stepCell threeLives (Cell True 1 0)))
 
 tests2 :: TestTree
 tests2 = testGroup "GameOfLifeTests"
   [
-    testCase "" $ testNeighbors
-    >> testStepBoard
+
+    testCase "testNeighbors" $ testNeighbors
+    , testCase "testStepBoard" $ testStepBoard
   ]
 
 runner = defaultMain tests2
