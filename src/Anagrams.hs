56871e2988e72c69494710f6afdefafb939a9359
jack <steve.a.shogren@gmail.com>
2019-03-01 15:27:32 -0500

    Populating a trie

--- src/Anagrams.hs
+++ src/Anagrams.hs
@@ -3,6 +3,7 @@ module Anagrams where
 import Data.List.Split(splitOn, wordsBy)
 import qualified System.IO as IO
 import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 
 data Trie = Node (M.Map Char Trie)
            | EmptyNode
@@ -14,8 +15,21 @@ dictionary = do
   return $ drop 7 $ words text
 
 populateMap :: String -> Trie -> Trie
-populateMap (letter:word) (Node root) =
-  Node $ M.insert letter (populateMap word (Node root)) root
-populateMap "" (Node root) = EmptyNode
+populateMap (letter:word) EmptyNode =
+  Node $ M.insert letter (populateMap word (Node M.empty)) M.empty
+populateMap (letter:word) (Node map) =
+  if M.member letter map then
+    let dic = fromMaybe EmptyNode $ M.lookup letter map
+        newMap = populateMap word dic
+        newOuter = M.insert letter newMap map
+    in Node newOuter
+  else
+    Node $ M.insert letter (populateMap word (Node M.empty)) map
+  -- in Node $ M.insert letter (populateMap word (Node dic)) dic
+populateMap "" map = map
 
-dict = populateMap "yes" (Node M.empty)
+dict :: IO Trie
+dict = do
+  words <- dictionary
+  return $ foldr (\word node -> populateMap word node) (EmptyNode) words --["abc", "ab3"]
+
